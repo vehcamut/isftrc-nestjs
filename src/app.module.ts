f73811bf500be902cc28b1deaea5e2ccb1eb4b61
Vehcamut <vehcamut@gmail.com>
2022-08-26 16:57:11 +0400

    Fixed database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASE_URL)],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
