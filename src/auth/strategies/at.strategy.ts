55879496858a82442566f8a6d8868567a7e5e615
Vehcamut <vehcamut@gmail.com>
2022-08-26 17:09:32 +0400

    Strategies for JWT tokens have been created

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'at-secret',
+    });
+  }
+
+  validate(payload: any) {
+    return payload;
+  }
+}
---
+++ src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'rt-secret',
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    return payload;
+  }
+}
