b396f5ae5ef9e9457758c1ba919d46fa9db327ed
Vehcamut <vehcamut@gmail.com>
2022-08-26 16:58:02 +0400

    Connected Validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+  app.useGlobalPipes(new ValidationPipe());
+  const PORT = process.env.PORT;
+
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
