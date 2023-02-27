9429997e108838570f5aa5ba913537beeeab6a5c
Vehcamut <vehcamut@gmail.com>
2023-02-27 19:06:44 +0400

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger/dist';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 const whitelist = [
